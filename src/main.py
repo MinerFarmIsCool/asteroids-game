dfcf969c2275cbab1ae788777f9e0b2e837b172e
MinerFarmIsCool <joshua_whelan@outlook.com>
2025-09-10 22:10:23 +1000

    Things: - Added circle base class. - Added player class -- Added drawing of the player class -- Made the player class look triangular but its actually a circle :0 - meow

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 import pygame
 from constants import *
+from player import *
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+clock = pygame.time.Clock()
+dt = 0
 
 
 def main():
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
 
         for event in pygame.event.get():
@@ -15,8 +20,11 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
