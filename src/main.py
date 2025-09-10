60f08908c54a1537fca2982c08fb29f93711b327
MinerFarmIsCool <joshua_whelan@outlook.com>
2025-09-10 22:45:23 +1000

    Add player rotation functionality and update constants

--- src/main.py
+++ src/main.py
@@ -2,9 +2,6 @@ import pygame
 from constants import *
 from player import *
 
-
-
-
 def main():
     pygame.init()
 
@@ -14,10 +11,12 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-
+    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
             
         screen.fill("black")
         player.draw(screen)
@@ -25,7 +24,6 @@ def main():
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
